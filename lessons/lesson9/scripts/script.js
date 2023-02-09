89bbdc41016997d1302322e14dd7f6cddd04867b
Slinick <119591833+Slinick@users.noreply.github.com>
2023-02-09 17:55:24 +0100

    home4

---
+++ lessons/lesson9/scripts/script.js
@@ -0,0 +1,26 @@
+// console.log("hello world");
+
+// // let name = prompt("write your name");
+// console.log("hello " + name);
+// console.log(`hello ${name}`);
+// console.log(5);
+// console.log("5");
+
+// console.log(typeof(name));
+// console.log(typeof(5));
+
+// const r = 122;
+// const g = 123;
+// const b = 124;
+// console.log(`rgb(${r},${g},${b})`);
+
+// let age = prompt("write your age");
+// console.log(age * 365*24*60);
+
+// let number = prompt("write your number");
+// console.log(`число в квадрате ${number*number}`);
+
+let number1 = prompt("write your number1");
+let number2 = prompt("write your number2");
+console.log(` ${+number1 + +number2}`);
+console.log(`${number1} + ${number2}`);
