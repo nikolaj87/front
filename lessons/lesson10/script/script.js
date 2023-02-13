aef6c4283729304e8ca6514606df3b76dbed8e83
Slinick <119591833+Slinick@users.noreply.github.com>
2023-02-13 13:58:49 +0100

    lesson10add

---
+++ lessons/lesson10/script/script.js
@@ -0,0 +1,46 @@
+// const value_1 = +prompt("type 1 number")
+// const value_2 = +prompt("type 2 number")
+// if(value_1 > value_2){
+//     console.log("Value 1 is bigger")
+// } else if (value_1 < value_2){
+//     console.log("Value 2 is bigger")
+// } else {
+//     console.log("is equal")
+// }
+//---------------------------------------------
+// const value_1 = +prompt("type 1 number")
+
+// if(value_1 > 0){
+//     console.log("Value 1 is bigger than zero")
+// } else if (value_1 < 0){
+//     console.log("less than 0")
+// } else {
+//     console.log("0")
+// }
+//==================================================
+// while(true){
+//     const value_1 = +prompt("type number only!");
+//     if(!isNaN(value_1)){
+//         console.log("можжно перевести");
+//         break;
+//     }
+// }
+
+// const value = +prompt("введите год")
+
+// if(value % 4 == 0 || value % 100 == 0 || value % 400 == 0){
+//     console.log("год высокостный")
+// } else {
+//     console.log("год не высокостный")
+// }
+//=================================================================
+const arr = [1,-2, 3, -4, 5, -6]
+// arr.push(5);
+// arr.pop
+
+// arr.unshift(0)
+// console.log(arr)
+
+for(i=arr.length-1; i >= 0; i--){
+        console.log(arr[i])
+}
